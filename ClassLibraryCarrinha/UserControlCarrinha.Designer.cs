45de41a449e6f6adcb056a808a54c6c41c7c5d2c
Nuno Ramalho <Nuno Ramalho>
2017-10-26 21:14:53 +0100

    Add project files.

---
+++ ClassLibraryCarrinha/UserControlCarrinha.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ClassLibraryCarrinha
+{
+    partial class UserControlCarrinha
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLugar1 = new System.Windows.Forms.Button();
+            this.buttonLugar2 = new System.Windows.Forms.Button();
+            this.buttonLugar3 = new System.Windows.Forms.Button();
+            this.buttonLugar4 = new System.Windows.Forms.Button();
+            this.buttonLugar5 = new System.Windows.Forms.Button();
+            this.buttonLugar6 = new System.Windows.Forms.Button();
+            this.buttonMotorista = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLugar1
+            // 
+            this.buttonLugar1.Location = new System.Drawing.Point(231, 72);
+            this.buttonLugar1.Name = "buttonLugar1";
+            this.buttonLugar1.Size = new System.Drawing.Size(75, 38);
+            this.buttonLugar1.TabIndex = 0;
+            this.buttonLugar1.Text = "1";
+            this.buttonLugar1.UseVisualStyleBackColor = true;
+            // 
+            // buttonLugar2
+            // 
+            this.buttonLugar2.Location = new System.Drawing.Point(231, 128);
+            this.buttonLugar2.Name = "buttonLugar2";
+            this.buttonLugar2.Size = new System.Drawing.Size(75, 38);
+            this.buttonLugar2.TabIndex = 1;
+            this.buttonLugar2.Text = "2";
+            this.buttonLugar2.UseVisualStyleBackColor = true;
+            // 
+            // buttonLugar3
+            // 
+            this.buttonLugar3.Location = new System.Drawing.Point(142, 72);
+            this.buttonLugar3.Name = "buttonLugar3";
+            this.buttonLugar3.Size = new System.Drawing.Size(73, 38);
+            this.buttonLugar3.TabIndex = 2;
+            this.buttonLugar3.Text = "3";
+            this.buttonLugar3.UseVisualStyleBackColor = true;
+            // 
+            // buttonLugar4
+            // 
+            this.buttonLugar4.Location = new System.Drawing.Point(142, 128);
+            this.buttonLugar4.Name = "buttonLugar4";
+            this.buttonLugar4.Size = new System.Drawing.Size(73, 38);
+            this.buttonLugar4.TabIndex = 3;
+            this.buttonLugar4.Text = "4";
+            this.buttonLugar4.UseVisualStyleBackColor = true;
+            // 
+            // buttonLugar5
+            // 
+            this.buttonLugar5.Location = new System.Drawing.Point(58, 72);
+            this.buttonLugar5.Name = "buttonLugar5";
+            this.buttonLugar5.Size = new System.Drawing.Size(78, 38);
+            this.buttonLugar5.TabIndex = 4;
+            this.buttonLugar5.Text = "5";
+            this.buttonLugar5.UseVisualStyleBackColor = true;
+            // 
+            // buttonLugar6
+            // 
+            this.buttonLugar6.Location = new System.Drawing.Point(58, 128);
+            this.buttonLugar6.Name = "buttonLugar6";
+            this.buttonLugar6.Size = new System.Drawing.Size(78, 38);
+            this.buttonLugar6.TabIndex = 5;
+            this.buttonLugar6.Text = "6";
+            this.buttonLugar6.UseVisualStyleBackColor = true;
+            // 
+            // buttonMotorista
+            // 
+            this.buttonMotorista.Location = new System.Drawing.Point(367, 72);
+            this.buttonMotorista.Name = "buttonMotorista";
+            this.buttonMotorista.Size = new System.Drawing.Size(56, 38);
+            this.buttonMotorista.TabIndex = 6;
+            this.buttonMotorista.Text = "Mot";
+            this.buttonMotorista.UseVisualStyleBackColor = true;
+            // 
+            // UserControlCarrinha
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::ClassLibraryCarrinha.Properties.Resources.bus;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Controls.Add(this.buttonMotorista);
+            this.Controls.Add(this.buttonLugar6);
+            this.Controls.Add(this.buttonLugar5);
+            this.Controls.Add(this.buttonLugar4);
+            this.Controls.Add(this.buttonLugar3);
+            this.Controls.Add(this.buttonLugar2);
+            this.Controls.Add(this.buttonLugar1);
+            this.DoubleBuffered = true;
+            this.Name = "UserControlCarrinha";
+            this.Size = new System.Drawing.Size(482, 260);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLugar1;
+        private System.Windows.Forms.Button buttonLugar2;
+        private System.Windows.Forms.Button buttonLugar3;
+        private System.Windows.Forms.Button buttonLugar4;
+        private System.Windows.Forms.Button buttonLugar5;
+        private System.Windows.Forms.Button buttonLugar6;
+        private System.Windows.Forms.Button buttonMotorista;
+    }
+}
---
+++ ClassLibraryCarrinha/UserControlCarrinha.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClassLibraryCarrinha
+{
+
+    public partial class UserControlCarrinha : UserControl
+    {
+        // Atributos privados
+        private bool motoristaAtribuido;
+        private int numLugaresLivres;
+        private int numLugaresReservados;
+
+        // Propriedades dos atributos
+        public bool MotoristaAtribuido        
+        {
+            get { return motoristaAtribuido; }
+            set { motoristaAtribuido = value; }
+        }
+     
+        public int NumLugaresLivres
+        {
+            get { return numLugaresLivres; }
+            set { numLugaresLivres = value; }
+        }
+
+        public int NumLugaresReservados
+        {
+            get { return numLugaresReservados; }
+            set { numLugaresReservados = value; }
+        }
+
+        // Construtor padrão
+        public UserControlCarrinha()
+        {
+            InitializeComponent();
+            Inicializar();
+            buttonLugar1.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonLugar2.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonLugar3.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonLugar4.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonLugar5.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonLugar6.Click += new EventHandler(QuandoBotaoLugarPressionado);
+            buttonMotorista.Click += new EventHandler(QuandoBotaoMotoristaPressionado);
+
+      
+        }
+
+
+
+        //-------------------------------------------------------------------------------------
+        // -----                       Método (Público)  BotaoClickHandler               ------
+        //-------------------------------------------------------------------------------------
+        // - Gerar um evento para deteção de clique nos botões
+        //-------------------------------------------------------------------------------------
+        public void BotaoClickHandler(EventHandler handler)
+        {
+            buttonLugar1.Click += handler;
+            buttonLugar2.Click += handler;
+            buttonLugar3.Click += handler;
+            buttonLugar4.Click += handler;
+            buttonLugar5.Click += handler;
+            buttonLugar6.Click += handler;
+            buttonMotorista.Click += handler;
+        }
+
+        //-------------------------------------------------------------------------------------
+        // -----                             Método Inicializar                          ------
+        //-------------------------------------------------------------------------------------
+        // - Coloca a interface no estado inicial
+        // - Inicializa os valores dos atributos
+        //-------------------------------------------------------------------------------------
+        private void Inicializar()
+        {
+            BotaoLivre(buttonLugar1);
+            BotaoLivre(buttonLugar2);
+            BotaoLivre(buttonLugar3);
+            BotaoLivre(buttonLugar4);
+            BotaoLivre(buttonLugar5);
+            BotaoLivre(buttonLugar6);
+            BotaoSemMotorista(buttonMotorista);
+            MotoristaAtribuido = false;
+            NumLugaresLivres = 6;
+            NumLugaresReservados = 0;
+
+        }
+
+
+
+        //-------------------------------------------------------------------------------------
+        // -----                       Método QuandoBotaoMotoristaPressionado            ------
+        //-------------------------------------------------------------------------------------
+        // - Inicializa a interface caso não exista motorista
+        // - Atualiza propriedade MotoristaAtribuido
+        //-------------------------------------------------------------------------------------
+        private void QuandoBotaoMotoristaPressionado(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if(btn.Text == "SIM")
+            {
+                Inicializar();
+            }
+            else
+            {
+                BotaoComMotorista(btn);
+                MotoristaAtribuido = true;
+            }
+        }
+        //-------------------------------------------------------------------------------------
+        // -----                       Método QuandoBotaoLugarPressionado            ------
+        //-------------------------------------------------------------------------------------
+        // - Testa se existe motorista para que se possam reservar lugares
+        // - Atualiza aspeto e texto dos botões
+        // - Atualiza as propriedades NumLugaresLivres e NumLugaresReservados
+        //-------------------------------------------------------------------------------------
+        public void QuandoBotaoLugarPressionado(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (!motoristaAtribuido)
+            {
+                MessageBox.Show("Primeiro deve atribuir um motorista.", "Informação",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Information);
+                return;
+            }
+
+            if (btn.Text == "Livre")
+            {
+                BotaoOcupado(btn);
+                NumLugaresLivres--;
+                NumLugaresReservados++;
+            }
+            else
+            {
+                BotaoLivre(btn);
+                NumLugaresLivres++;
+                NumLugaresReservados--;
+            }
+        }
+        //-------------------------------------------------------------------------------------
+        // -----                       Método BotaoSemMotorista            ------
+        //-------------------------------------------------------------------------------------
+        // -  Atualiza aspeto e texto do botão relativo ao motorista - Sem Motorista
+        //-------------------------------------------------------------------------------------       
+        private void BotaoSemMotorista(Button btn)
+        {
+            btn.Text = "NÃO";
+            btn.BackColor = Color.Black;
+            btn.ForeColor = Color.White;
+        }
+        //-------------------------------------------------------------------------------------
+        // -----                       Método BotaoComMotorista            ------
+        //-------------------------------------------------------------------------------------
+        // -  Atualiza aspeto e texto do botão relativo ao motorista - Com Motorista
+        //------------------------------------------------------------------------------------- 
+        private void BotaoComMotorista(Button btn)
+        {
+            btn.Text = "SIM";
+            btn.BackColor = Color.Gray;
+            btn.ForeColor = Color.Black;
+        }
+        //-------------------------------------------------------------------------------------
+        // -----                       Método BotaoLivre                                ------
+        //-------------------------------------------------------------------------------------
+        // -  Atualiza aspeto e texto dos botões relativos aos passageiros - Livre
+        //------------------------------------------------------------------------------------- 
+        private void BotaoLivre(Button btn)
+        {
+            btn.Text = "Livre";
+            btn.BackColor = Color.LawnGreen;
+        }
+        //-------------------------------------------------------------------------------------
+        // -----                       Método BotaoOcupado                                ------
+        //-------------------------------------------------------------------------------------
+        // -  Atualiza aspeto e texto dos botões relativos aos passageiros - Ocupado
+        //------------------------------------------------------------------------------------- 
+        private void BotaoOcupado(Button btn)
+        {
+            btn.Text = "ocupado";
+            btn.BackColor = Color.IndianRed;
+        }
+    }
+}
